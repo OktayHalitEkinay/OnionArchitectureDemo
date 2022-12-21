636c0cbc37964248dd418fb2fc35285510b5996b
OktayHalitEkinay <67113059+OktayHalitEkinay@users.noreply.github.com>
2022-12-21 22:11:13 +0300

    aa

---
+++ src/Core/Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntitiy
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> AddAsync(T entity);
+
+    }
+}
