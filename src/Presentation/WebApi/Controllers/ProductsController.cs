477a7932e3f68b211356d313ea9ceb8f6a1d4ac7
OktayHalitEkinay <67113059+OktayHalitEkinay@users.noreply.github.com>
2022-12-22 21:24:24 +0300

    aa

---
+++ src/Presentation/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var result=await _productRepository.GetAllAsync();
+            return Ok(result);
+        }
+
+    }
+}
